0be4462741698978cb8ac1e06425ae72d920762c
Welch <thisiswelch@protonmail.com>
2022-10-02 08:09:30 +0800

    Section 3: Using react hooks -useState

--- ReStore/client/src/App.tsx
+++ ReStore/client/src/App.tsx
@@ -1,18 +1,26 @@
-const products = [
-  {name: 'product1', price: 100.00},
-  {name: 'product2', price: 200.00},
-  {name: 'product2', price: 200.00}
-]
+import { useState } from "react";
 
 function App() {
+  // Use state
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.00 },
+    { name: 'product2', price: 200.00 }
+  ]);
+
+  function addProduct() {
+    setProducts(prevState => [...prevState,
+    { name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100 }])
+  }
+
   return (
     <div>
       <h1>Re-Store</h1>
       <ul>
-        {products.map((item, index)=> (
+        {products.map((item, index) => (
           <li key={index}>{item.name} = {item.price}</li>
         ))}
       </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   );
 }

23e6ff504ed49744c61b5337ece41db112df0935
Welch <thisiswelch@protonmail.com>
2022-10-02 08:27:04 +0800

    Section 3: Adding CORS configuration to the API

--- ReStore/client/src/App.tsx
+++ ReStore/client/src/App.tsx
@@ -7,6 +7,10 @@ function App() {
     { name: 'product2', price: 200.00 }
   ]);
 
+  // Adding the empty array dependency here is important as this will prevent
+  // the infinite call to api.  What will happen when there is no dependency is
+  // every time the state (on this case) is updated and will cause to re-render
+  // useEffect hook will trigger again if there is no dependency
   useEffect(() => {
     fetch('http://localhost:5000/api/products')
     .then(response => response.json())

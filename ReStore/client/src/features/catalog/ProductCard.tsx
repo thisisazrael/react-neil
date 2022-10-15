f804eb2fb41464dfb1feed4b93d89e65cea99dcc
Welch <thisiswelch@protonmail.com>
2022-10-15 16:07:42 +0800

    Section 3: Styling the product card

--- ReStore/client/src/features/catalog/ProductCard.tsx
+++ ReStore/client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface Props {
@@ -8,24 +8,34 @@ interface Props {
 export default function ProductCard({ product }: Props) {
     return (
         <Card>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{bgcolor: 'secondary.main'}}>
+                        {product.name.charAt(0).toUpperCase()}
+                    </Avatar>
+                }
+                title={product.name}
+                titleTypographyProps={{
+                    // sx has access to theme's variables. e.g. color:  'primary.main'
+                    sx: {fontWeight: 'bold', color: 'primary.main'}
+                }}
+            />
             <CardMedia
-                component="img"
-                height="140"
-                image="http://picsum.photos/200"
-                alt="green iguana"
+                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
+                image={product.pictureUrl}
+                title={product.name}
             />
             <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+                <Typography gutterBottom color='secondary' variant="h5" component="div">
+                    ${(product.price / 100).toFixed(2)}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
+                    {product.brand} / {product.type}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+                <Button size="small">Add to cart</Button>
+                <Button size="small">View</Button>
             </CardActions>
         </Card>
     )

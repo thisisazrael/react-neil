4302715ac9b7d09bf0d6897a769c83851b8ae634
Welch <thisiswelch@protonmail.com>
2022-10-16 01:11:25 +0800

    Section 3: Using Material UI Theme

--- ReStore/client/src/app/layout/App.tsx
+++ ReStore/client/src/app/layout/App.tsx
@@ -3,46 +3,28 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { Container, CssBaseline } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from 'react';
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from './Header';
 
 function App() {
-  // Use state
-  const [products, setProducts] = useState<Product[]>([]);
-
-  // Adding the empty array dependency here is important as this will prevent
-  // the infinite call to api.  What will happen when there is no dependency is
-  // every time the state (on this case) is updated and will cause to re-render
-  // useEffect hook will trigger again if there is no dependency
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-  function addProduct() {
-    setProducts(prevState => [...prevState,
-    {
-      id: prevState.length + 101,
-      name: 'product' + (prevState.length + 1),
-      price: (prevState.length * 100) + 100,
-      brand: 'some brand',
-      description: 'some description',
-      pictureUrl: 'http://picsum.photos/200'
-    }])
-  }
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType
+    }
+  })
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 

256c8e66b4a578bb69d739ba9680ef1843cd1f9b
Welch <thisiswelch@protonmail.com>
2022-11-27 15:35:59 +0800

    Section 4: Adding Nav Links to the AppBar

--- ReStore/client/src/index.tsx
+++ ReStore/client/src/index.tsx
@@ -9,11 +9,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
